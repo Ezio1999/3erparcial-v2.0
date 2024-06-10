203549dabcf23725160e2e01ac399d9e84301fee
Castillo <=joseignacionavacastillo@gmail.com>
2024-06-10 10:50:23 -0600

    :)

---
+++ practica54.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x,f=1;
+    cout << "ingrese un numero con 2 decimales: "<< endl;
+    cin >> x;
+    for ( float i = 1  ; i <= x; i++){
+        f = f*i;
+    }
+    cout << "El factorial del numero es: " << f;
+
+    return 0;
+}
