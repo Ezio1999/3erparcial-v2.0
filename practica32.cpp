23730d7865f77c67aae69520cb0a608b88f6d5bf
Castillo <=joseignacionavacastillo@gmail.com>
2024-06-13 12:15:16 -0600

    :)

---
+++ practica32.cpp
@@ -0,0 +1,19 @@
+/*Un programa que digite 5 números y que diga en que posicion estan los 5 numeros*/
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num[5];
+    int i, numero;
+    for (i = 0; i <= 5-1; i++)
+    {
+        cout << "Digite un numero para la posocion: \n";
+        cin >> numero;
+        num[i] = numero;
+    }
+    for (i = 0; i <= 5-1; i++)
+    {
+        cout << "El dato  en la posicion " << i << " es: " << num[i] << endl;
+    }
+    return 0;
+}
---
+++ practica57.cpp
@@ -0,0 +1,31 @@
+/*Cruud*/
+#include <iostream>
+
+using namespace std;
+const int max_usuarios = 100
+
+
+void regitrous (){
+    cout << "Ingrese el nombre del usuario: ";
+    cin >> usuario;
+}
+
+void eliminarus(){
+}
+
+void mostrarus(){
+}
+
+
+
+//Función principal
+int main(){
+    int opcion;
+    do
+    {
+        
+    } while ();
+    
+
+    return 0;
+}
