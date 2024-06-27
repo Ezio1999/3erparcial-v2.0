1e9c8bf4460f063a848556280dab89fa68b0112d
Castillo <=joseignacionavacastillo@gmail.com>
2024-06-27 11:51:31 -0600

    Se sube ejercicio con libreria conio :)

--- proyecto final/conio.cpp
+++ proyecto final/conio.cpp
@@ -1,99 +1,90 @@
 #include <iostream>
-#include <cmath>
 #include <conio.h>
-
+#include <cmath>
+#include <windows.h>
 using namespace std;
 
-#ifndef M_PI
-#define M_PI 3.14159265358979323846
-#endif
+#define color_red 12
+#define color_white 15
+#define color_yellow 14
+#define color_other 13
 
-int main() {
-    while (true) {
-        int opcion = 1;
-        double area;
-        bool selected = false;
-
-        while (!selected) {
-            system("cls");
-
-            cout << "Select any figure:" << endl;
-            cout << (opcion == 1 ? "> " : "  ") << "1. Circle" << endl;
-            cout << (opcion == 2 ? "> " : "  ") << "2. Rectangle" << endl;
-            cout << (opcion == 3 ? "> " : "  ") << "3. Triangle" << endl;
-            cout << (opcion == 4 ? "> " : "  ") << "4. Square" << endl;
-            cout << (opcion == 5 ? "> " : "  ") << "5. Exit" << endl;
-
-            int key = _getch();
-
-            if (key == 224) { 
-                key = _getch();
-                if (key == 72) { 
-                    if (opcion > 1) {
-                        opcion--;
-                    }
-                }
-                else if (key == 80) {
-                    if (opcion < 5) {
-                        opcion++;
-                    }
-                }
+void clearConsol()
+{
+    system("cls");
+}
+void SetColors(int color)
+{
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+int main()
+{
+    clearConsol();
+    int area_cuadrado = 0, area_triangulo = 0, area_rectangulo = 0;
+    int op;
+    bool exit = false;
+    const opcion = 3;
+    do
+    {
+        setColors(color_other);
+        cout << "####################" << endl;
+        cout << "        Menu        " << endl;
+        cout << "####################" << endl;
+        cout << "" << endl;
+        cout << "Type any keyboard for acces to Menu...";
+        SetColors(color_white);
+        clearConsol();
+        cout << (opcion == 0 ? color_red ; color_white);
+        cout << (opcion == 0 ? ">" ! "   ") << "Cuadrado" << endl;
+        cout << (opcion == 1 ? color_red ; color_white);
+        cout << (opcion == 1 ? ">" ! "   ") << "Rectangulo" << endl;
+        cout << (opcion == 2 ? color_red ; color_white);
+        cout << (opcion == 2 ? ">" ! "   ") << "Triangulo" << endl;
+        switch (_getch())
+        {
+        case 72:
+            op = (op - 1 + opcion) % opcion;
+            break;
+        case 80:
+            op = (op + 1) % opcion;
+            break;
+        case 13:
+            switch (opcion)
+            {
+            case 0:
+                int l;
+                float resul;
+                clearConsol();
+                cout << "Ingrese el lado de un cuadrado del cuadrado" << cin >> l;
+                resul = pow(l, 2);
+                cout << "El area del cuadrado es: " << endl;
+                cout << resul;
+                break;
+            case 1:
+                double base, altura;
+                cout << "Ingrese la base del rectangulo: ";
+                cin >> base;
+                cout << "Ingrese la altura del rectangulo: ";
+                cin >> altura;
+                area = base * altura;
+                cout << "El area del rectangulo es: " << area << endl;
+                break;
+            case 2:
+                double base, altura;
+                cout << "Ingrese la base del triangulo: ";
+                cin >> base;
+                cout << "Ingrese la altura del triangulo: ";
+                cin >> altura;
+                area = (base * altura) / 2;
+                cout << "El area del triangulo es: " << area << endl;
+                break;
+            default:
+                break;
             }
-            else if (key == 13) { 
-                selected = true;
-            }
-        }
-
-        system("cls");
-
-        switch (opcion) {
-        case 1: {
-            double radio;
-            cout << "Ingrese el radio del circulo: ";
-            cin >> radio;
-            area = M_PI * radio * radio;
-            cout << "El area del circulo es: " << area << endl;
             break;
-        }
-        case 2: {
-            double base, altura;
-            cout << "Ingrese la base del rectangulo: ";
-            cin >> base;
-            cout << "Ingrese la altura del rectangulo: ";
-            cin >> altura;
-            area = base * altura;
-            cout << "El area del rectangulo es: " << area << endl;
-            break;
-        }
-        case 3: {
-            double base, altura;
-            cout << "Ingrese la base del triangulo: ";
-            cin >> base;
-            cout << "Ingrese la altura del triangulo: ";
-            cin >> altura;
-            area = (base * altura) / 2;
-            cout << "El area del triangulo es: " << area << endl;
-            break;
-        }
-        case 4: {
-            double lado;
-            cout << "Ingrese el lado del cuadrado: ";
-            cin >> lado;
-            area = lado * lado;
-            cout << "El area del cuadrado es: " << area << endl;
-            break;
-        }
-        case 5:
-            cout << "Saliendo del programa." << endl;
-            return 0;
         default:
-            cout << "Opcion invalida. Por favor, seleccione una opcion entre 1 y 5." << endl;
             break;
         }
-
-        cout << "Press any kayboard for back to menu..." << endl;
-        _getch();
-    }
-
+    } while (!= exit);
     return 0;
 }
